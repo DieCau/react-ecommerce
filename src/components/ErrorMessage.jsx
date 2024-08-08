c7a0387698d21944679b4889cbd3616e498191ee
Diego Caucota <diegocaucota@gmail.com>
2024-08-08 08:12:21 -0300

    add: ErrorMessage.jsx

---
+++ src/components/ErrorMessage.jsx
@@ -0,0 +1,9 @@
+export const ErrorMessage = () => {
+  return (
+    <>
+      <div className="w-100 vh-100 text-center">
+        <h1>Lo sentimos, ha ocurrido un error! Intente de nuevo mas tarde...</h1>
+      </div>
+    </>
+  );
+};
